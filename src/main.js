05fd642c5dcbd75370d329a0f09cf06dc92175d0
梁亮 <liang.liang@cctrend.com.cn>
2021-03-14 10:09:25 +0800

    Initialize the globe

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
+import VueCesium from 'vue-cesium'
+import lang from 'vue-cesium/lang/zh-hans'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(VueCesium, {
+  cesiumPath: 'https://cdn.jsdelivr.net/npm/iclient3d-10i@1.2.0/vendor/Cesium/Cesium.js',
+  lang: lang
+})
 
 new Vue({
   render: h => h(App),
